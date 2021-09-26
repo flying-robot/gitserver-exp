810786150d620cb1aafb21181e543251c150a807
Alan Harris <71571391+flying-robot@users.noreply.github.com>
2021-09-26 07:58:59 -0400

    Implement a tracing adapter for Git.
    This decorates the normal adapter with goodies.

---
+++ adapter/git/tracing_git.go
@@ -0,0 +1,53 @@
+package git
+
+import (
+	"context"
+
+	"github.com/flying-robot/gitserver/service"
+)
+
+// A TracingGitAdapter delegates to a normal GitAdapter, but with Git's debugging
+// environment variables configured for more verbose output.
+type TracingGitAdapter struct {
+	Trace            bool
+	TracePackAccess  bool
+	TracePacket      bool
+	TracePerformance bool
+	TraceSetup       bool
+
+	GitAdapter
+}
+
+// Init creates an empty Git repository or reinitializes an existing one.
+func (t *TracingGitAdapter) Init(ctx context.Context, args service.InitArgs) ([]byte, error) {
+	args.Env = append(args.Env, t.env()...)
+	return t.GitAdapter.Init(ctx, args)
+}
+
+// Fetch downloads objects and refs from another repository.
+func (t *TracingGitAdapter) Fetch(ctx context.Context, args service.FetchArgs) ([]byte, error) {
+	args.Env = append(args.Env, t.env()...)
+	return t.GitAdapter.Fetch(ctx, args)
+}
+
+// env returns a slice of environment variables that configure Git to produce
+// more verbose debugging output.
+func (t *TracingGitAdapter) env() []string {
+	var env []string
+	if t.Trace {
+		env = append(env, "GIT_TRACE=true")
+	}
+	if t.TracePackAccess {
+		env = append(env, "GIT_TRACE_PACK_ACCESS=true")
+	}
+	if t.TracePacket {
+		env = append(env, "GIT_TRACE_PACKET=true")
+	}
+	if t.TracePerformance {
+		env = append(env, "GIT_TRACE_PERFORMANCE=true")
+	}
+	if t.TraceSetup {
+		env = append(env, "GIT_TRACE_SETUP=true")
+	}
+	return env
+}
--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/flying-robot/gitserver/adapter/filesystem"
 	"github.com/flying-robot/gitserver/adapter/git"
@@ -10,14 +11,41 @@ import (
 )
 
 func main() {
-	cloneRepoService := &service.CloneRepositoryService{
-		Filesystem: &filesystem.FilesystemAdapter{},
-		Git:        &git.GitAdapter{},
+	log.Println("cloning with default adapters")
+	{
+		fmt.Println("=================================================")
+		cloneRepoService := &service.CloneRepositoryService{
+			Filesystem: &filesystem.FilesystemAdapter{},
+			Git:        &git.GitAdapter{},
+		}
+		out, err := cloneRepoService.Clone(context.Background(), service.CloneRequest{
+			Upstream: "https://github.com/flying-robot/commit-sink.git",
+			Local:    "/tmp/commit-sink.git",
+		})
+		fmt.Println(string(out))
+		fmt.Println(err)
+		fmt.Println()
 	}
-	out, err := cloneRepoService.Clone(context.Background(), service.CloneRequest{
-		Upstream: "https://github.com/flying-robot/commit-sink.git",
-		Local:    "/tmp/commit-sink.git",
-	})
-	fmt.Println(string(out))
-	fmt.Println(err)
+
+	log.Println("cloning with tracing adapters")
+	{
+		fmt.Println("=================================================")
+		cloneRepoService := &service.CloneRepositoryService{
+			Filesystem: &filesystem.FilesystemAdapter{},
+			Git: &git.TracingGitAdapter{
+				Trace:            true,
+				TracePackAccess:  true,
+				TracePacket:      true,
+				TracePerformance: true,
+				TraceSetup:       true,
+			},
+		}
+		out, err := cloneRepoService.Clone(context.Background(), service.CloneRequest{
+			Upstream: "https://github.com/flying-robot/commit-sink.git",
+			Local:    "/tmp/commit-sink.git",
+		})
+		fmt.Println(string(out))
+		fmt.Println(err)
+		fmt.Println()
+	}
 }
