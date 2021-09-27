2dfbf488cfa2648e625c09411755058cb9e8fcd1
Alan Harris <71571391+flying-robot@users.noreply.github.com>
2021-09-27 08:25:34 -0400

    Rename WriteWrapper to Writer.

--- service/types.go
+++ service/types.go
@@ -5,17 +5,17 @@ import (
 	"os"
 )
 
-// A WriteWrapper allows output streams to be wrapped with useful functionality.
-type WriteWrapper interface {
+// A Writer allows output streams to be wrapped with useful functionality.
+type Writer interface {
 	Writer(w io.Writer) io.Writer
 }
 
 // BaseArgs are provided to every subcommand and control the operating environment.
 type BaseArgs struct {
-	Dir      string       // The working directory of the subcommand.
-	Env      []string     // The key-value environment values, if any.
-	FlowRate int64        // The write rate for Stdout.
-	Stdout   WriteWrapper // The standard output stream, potentially modified.
+	Dir      string   // The working directory of the subcommand.
+	Env      []string // The key-value environment values, if any.
+	FlowRate int64    // The write rate for Stdout.
+	Stdout   Writer   // The standard output stream, potentially modified.
 }
 
 // InitArgs configure the behavior of the Init subcommand.
@@ -37,7 +37,7 @@ type MkdirAllArgs struct {
 
 // A CloneRequest is provided as input to the CloneRepository service.
 type CloneRequest struct {
-	Upstream string       // The location of the repository to fetch from.
-	Local    string       // The location where the repository will reside.
-	Stdout   WriteWrapper // A flow rate limiter to conserve bandwidth.
+	Upstream string // The location of the repository to fetch from.
+	Local    string // The location where the repository will reside.
+	Stdout   Writer // A flow rate limiter to conserve bandwidth.
 }
